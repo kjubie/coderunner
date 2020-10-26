97d67c15d16acdc8faf5a1a32ee3f8fddf597da6
Sabrina <sabsi24@gmx.at>
2020-10-26 15:52:53 +0100

    angular pages added

---
+++ src/FHTW.CodeRunner.Services/ClientApp/src/app/exercise-collection/exercise-collection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-collection',
+  templateUrl: './exercise-collection.component.html',
+  styleUrls: ['./exercise-collection.component.css']
+})
+export class ExerciseCollectionComponent {
+
+  
+}
