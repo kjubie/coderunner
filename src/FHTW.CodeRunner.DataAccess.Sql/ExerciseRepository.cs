9f8dd186e657b472dedaab70aa0ed3a2e4a84ba0
Roman <roman.schuh98@hotmail.de>
2020-11-03 20:31:38 +0100

    reworked getExerciseById function

--- src/FHTW.CodeRunner.DataAccess.Sql/ExerciseRepository.cs
+++ src/FHTW.CodeRunner.DataAccess.Sql/ExerciseRepository.cs
@@ -1,5 +1,6 @@
 ﻿using FHTW.CodeRunner.DataAccess.Entities;
 using FHTW.CodeRunner.DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 
@@ -13,7 +14,14 @@ namespace FHTW.CodeRunner.DataAccess.Sql
 
         Exercise IExerciseRepository.GetExerciseById(int id)
         {
-            return context.Exercise.Single(exercise => exercise.Id == id);
+            return context.Exercise
+                    .Include(e => e.FkUser)
+                    .Include(e => e.ExerciseVersion.OrderByDescending(v => v.VersionNumber).First())
+                        .ThenInclude(v => v.FkUser)
+                    .Include(e => e.ExerciseVersion.OrderByDescending(v => v.VersionNumber).First())
+                        .ThenInclude(v => v.ExerciseLanguage)
+                            .ThenInclude(el => el.FkWrittenLanguage)
+                    .Single(e => e.Id == id);
             //throw new NotImplementedException();
         }
     }
