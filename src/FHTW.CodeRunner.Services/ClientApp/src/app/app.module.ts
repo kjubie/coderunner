5e5e1409c8659767fe5209ab4fe7f8628c04bfe8
Sabrina <sabsi24@gmx.at>
2020-11-15 17:53:07 +0100

    removed old components from project

--- src/FHTW.CodeRunner.Services/ClientApp/src/app/app.module.ts
+++ src/FHTW.CodeRunner.Services/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from './login/login.component';
 import { ExerciseCollectionComponent } from './exercise-collection/exercise-collection.component';
 import { ExerciseCreateComponent } from './exercise-create/exercise-create.component';
