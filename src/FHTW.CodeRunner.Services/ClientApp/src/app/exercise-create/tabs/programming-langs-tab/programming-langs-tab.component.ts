f16b94c3ee14bb0d01c390e2866ef94baef9e50a
Sabrina Aigensberger <if18n133@technikum-wien.at>
2021-01-23 11:04:48 +0100

    syntax highlighting added to markdown

--- src/FHTW.CodeRunner.Services/ClientApp/src/app/exercise-create/tabs/programming-langs-tab/programming-langs-tab.component.ts
+++ src/FHTW.CodeRunner.Services/ClientApp/src/app/exercise-create/tabs/programming-langs-tab/programming-langs-tab.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { EditorInstance, EditorOption } from "angular-markdown-editor";
+import { MarkdownService } from "ngx-markdown";
 import { ExerciseBody } from "src/app/data-objects/create-exercise/exercise-body";
 
 @Component({
@@ -6,10 +8,51 @@ import { ExerciseBody } from "src/app/data-objects/create-exercise/exercise-body
     templateUrl: './programming-langs-tab.component.html',
     styleUrls: ['../../exercise-create.component.css']
 })
-export class ProgrammingLangsTabComponent {
+export class ProgrammingLangsTabComponent implements OnInit {
 
     @Input() exerciseBody: ExerciseBody;
     @Input() writtenLang: string;
 
-    hidePreview() { console.log('test'); }
+    bsEditorInstance: EditorInstance;
+    editorOptions: EditorOption;
+
+    constructor(private markdownService: MarkdownService) {}
+
+    ngOnInit() {
+        this.editorOptions = {
+            autofocus: false,
+            iconlibrary: 'fa',
+            savable: false,
+            onFullscreenExit: (e) => this.hidePreview(e),
+            onShow: (e) => this.bsEditorInstance = e,
+            parser: (val) => this.parse(val)
+        };
+    }
+
+    /** highlight all code found, needs to be wrapped in timer to work properly */
+    highlight() {
+        setTimeout(() => {
+        this.markdownService.highlight();
+        });
+    }
+
+    hidePreview(e) {
+        if (this.bsEditorInstance && this.bsEditorInstance.hidePreview) {
+        this.bsEditorInstance.hidePreview();
+        }
+    }
+
+    showFullScreen(isFullScreen: boolean) {
+        if (this.bsEditorInstance && this.bsEditorInstance.setFullscreen) {
+        this.bsEditorInstance.showPreview();
+        this.bsEditorInstance.setFullscreen(isFullScreen);
+        }
+    }
+
+    parse(inputValue: string) {
+        const markedOutput = this.markdownService.compile(inputValue.trim());
+        this.highlight();
+
+        return markedOutput;
+    }
 }
--- src/FHTW.CodeRunner.Services/ClientApp/src/app/exercise-create/tabs/written-lang-tab/written-lang-tab.component.ts
+++ src/FHTW.CodeRunner.Services/ClientApp/src/app/exercise-create/tabs/written-lang-tab/written-lang-tab.component.ts
@@ -1,12 +1,58 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
+import { EditorInstance, EditorOption } from "angular-markdown-editor";
+import { MarkdownService } from "ngx-markdown";
 import { ExerciseLanguage } from "src/app/data-objects/create-exercise/exercise-language";
 
 @Component({
     selector: 'written-lang-tab',
     templateUrl: './written-lang-tab.component.html',
+    encapsulation: ViewEncapsulation.None,
     styleUrls: ['../../exercise-create.component.css']
 })
-export class WrittenLangTabComponent {
+export class WrittenLangTabComponent implements OnInit {
 
     @Input() exerciseLanguage: ExerciseLanguage;
+
+    bsEditorInstance: EditorInstance;
+    editorOptions: EditorOption;
+
+    constructor(private markdownService: MarkdownService) {}
+
+    ngOnInit() {
+        this.editorOptions = {
+          autofocus: false,
+          iconlibrary: 'fa',
+          savable: false,
+          onFullscreenExit: (e) => this.hidePreview(e),
+          onShow: (e) => this.bsEditorInstance = e,
+          parser: (val) => this.parse(val)
+        };
+    }
+
+    /** highlight all code found, needs to be wrapped in timer to work properly */
+    highlight() {
+        setTimeout(() => {
+        this.markdownService.highlight();
+        });
+    }
+
+    hidePreview(e) {
+        if (this.bsEditorInstance && this.bsEditorInstance.hidePreview) {
+        this.bsEditorInstance.hidePreview();
+        }
+    }
+
+    showFullScreen(isFullScreen: boolean) {
+        if (this.bsEditorInstance && this.bsEditorInstance.setFullscreen) {
+        this.bsEditorInstance.showPreview();
+        this.bsEditorInstance.setFullscreen(isFullScreen);
+        }
+    }
+
+    parse(inputValue: string) {
+        const markedOutput = this.markdownService.compile(inputValue.trim());
+        this.highlight();
+
+        return markedOutput;
+    }
 }
