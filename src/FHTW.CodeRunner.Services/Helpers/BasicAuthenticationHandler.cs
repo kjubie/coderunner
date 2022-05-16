ee7bf1cbc81b9ddaad13d5a950373a2b3c6989ea
kjubie <65290601+kjubie@users.noreply.github.com>
2022-05-16 14:48:24 +0200

    Update BasicAuthenticationHandler.cs

--- src/FHTW.CodeRunner.Services/Helpers/BasicAuthenticationHandler.cs
+++ src/FHTW.CodeRunner.Services/Helpers/BasicAuthenticationHandler.cs
@@ -60,7 +60,7 @@ namespace FHTW.CodeRunner.Services.Helpers
 
             if (!this.Request.Headers.ContainsKey("Authorization"))
             {
-                return AuthenticateResult.Fail(Request);
+                return AuthenticateResult.Fail(this.Request.toString());
             }
 
             int? result = null;
